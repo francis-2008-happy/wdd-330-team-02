3a19c9c91a333b1aa4105a48d10cfe268496abe2
svitlanabradley <svitlana.bradley@gmail.com>
2025-07-10 12:57:44 +0200

    completed team activity week2

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,9 +1,9 @@
 // wrapper for querySelector...returns matching element
-export function qs(selector, parent = document) {
-  return parent.querySelector(selector);
-}
+// export function qs(selector, parent = document) {
+//   return parent.querySelector(selector);
+// }
 // or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
+export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
@@ -13,7 +13,7 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
-// set a listener for both touchend and click
+// // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
     event.preventDefault();
@@ -22,11 +22,7 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
-// export function getParam(param) {
-//   const queryString = window.location.search;
-//   const urlParams = new URLSearchParams(queryString);
-//   const product = urlParams.get('product');
-//   return product;
+
 
 export function getParam(param) {
     const queryString = window.location.search;
