c24799322449fd1fe6e3318da47fa1f241e82dca
francis-2008-happy <francisbucci2@gmail.com>
2025-07-16 19:18:39 +0100

    Dynamic Header and Footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
+import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
+
+
+loadHeaderFooter();
+
 const dataSource = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
+const productList = new ProductList("Tents", dataSource, listElement);
 
-const productList = new ProductList("tents", dataSource, listElement);
 productList.init();
+
+
