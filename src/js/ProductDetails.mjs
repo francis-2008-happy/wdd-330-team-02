510539658b3b7fb32f95a274549f7ca0d94ad6e9
francis-2008-happy <francisbucci2@gmail.com>
2025-07-17 23:48:34 +0100

    Expand Inventory

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,38 +1,112 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
-    constructor(productId, dataSource) {
-        this.productId = productId;
-        this.dataSource = dataSource;
-        this.roduct = {};
-    }
 
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
 
-    async init() {
-        this.product = await this.dataSource.findProductById(this.productId);
-        this.renderProductDetails();
-
-        document.getElementById('addToCart').addEventListener('click', this.addProductToCart.bind(this));
-    }
-
-    renderProductDetails() {
-        if (!this.product) return;
-
-        document.querySelector('.product-detail_brand').textContent = this.product.Brand.Name;
-        document.querySelector('.product-detail_name').textContent = this.product.NameWithoutBrand;
-        document.querySelector('.product-detail_image').src = this.product.Image;
-        document.querySelector('.product-detail_image').alt = this.product.Name;
-        document.querySelector('.product-card__price').textContent = `$${this.product.ListPrice}`;
-        document.querySelector('.product__color').textContent = this.product.Colors.ColorName;
-        document.querySelector('.product__description').textContent = this.product.Description;
-    }
+  async init() {
     
-    addProductToCart() {
-        let cartItems = getLocalStorage('so-cart') || [];
-        cartItems.push(this.product);
-        setLocalStorage('so-cart', cartItems);
-    }
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+
+    document
+      .getElementById("add-to-cart")
+      .addEventListener("click", this.addProductToCart.bind(this));
+  }
+
+  addProductToCart() {
+    const cartItems = getLocalStorage("so-cart") || [];
+    cartItems.push(this.product);
+    setLocalStorage("so-cart", cartItems);
+  }
+
+  renderProductDetails() {
+    productDetailsTemplate(this.product);
+  }
 }
+
+function productDetailsTemplate(product) {
+  document.querySelector("h2").textContent = product.Category.charAt(0).toUpperCase() + product.Category.slice(1);
+  document.querySelector("#p-brand").textContent = product.Brand.Name;
+  document.querySelector("#p-name").textContent = product.NameWithoutBrand;
+
+  const productImage = document.querySelector("#p-image");
+  productImage.src = product.Images.PrimaryExtraLarge;
+  productImage.alt = product.NameWithoutBrand;
+  const euroPrice = new Intl.NumberFormat('de-DE',
+    {
+      style: 'currency', currency: 'EUR',
+    }).format(Number(product.FinalPrice) * 0.85);
+  document.querySelector("#p-price").textContent = `${euroPrice}`;
+  document.querySelector("#p-color").textContent = product.Colors[0].ColorName;
+  document.querySelector("#p-description").innerHTML = product.DescriptionHtmlSimple;
+
+  document.querySelector("#add-to-cart").dataset.id = product.Id;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//       import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+
+// export default class ProductDetails {
+//     constructor(productId, dataSource) {
+//         this.productId = productId;
+//         this.dataSource = dataSource;
+//         this.roduct = {};
+//     }
+
+
+//     async init() {
+//         this.product = await this.dataSource.findProductById(this.productId);
+//         this.renderProductDetails();
+
+//         document
+//             .getElementById('addToCart')
+//             .addEventListener('click', this.addProductToCart.bind(this));
+//     }
+
+//     renderProductDetails() {
+//         if (!this.product) return;
+
+//         document.querySelector('.product-detail_brand').textContent = this.product.Brand.Name;
+//         document.querySelector('.product-detail_name').textContent = this.product.NameWithoutBrand;
+//         document.querySelector('.product-detail_image').src = this.product.Image;
+//         document.querySelector('.product-detail_image').alt = this.product.Name;
+//         document.querySelector('.product-card__price').textContent = `$${this.product.ListPrice}`;
+//         document.querySelector('.product__color').textContent = this.product.Colors.ColorName;
+//         document.querySelector('.product__description').textContent = this.product.Description;
+//     }
+    
+//     addProductToCart() {
+//         let cartItems = getLocalStorage('so-cart') || [];
+//         cartItems.push(this.product);
+//         setLocalStorage('so-cart', cartItems);
+//     }
+// }
 
 
 
