247919de681e1b40af3d3612cfcc301015781db4
svitlanabradley <svitlana.bradley@gmail.com>
2025-07-26 19:31:48 +0200

    completed team activity week 4

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -45,20 +45,23 @@ export default class CheckoutProcess {
   calculateItemSummary() {
     // calculate and display the total amount of the items in the cart, and the number of items.
     const summaryElement = document.querySelector(
-      this.outputSelector + " #cartTotal"
+      this.outputSelector + " #subtotal"
     );
     const itemNumElement = document.querySelector(
       this.outputSelector + " #num-items"
     );
-    itemNumElement.innerText = this.list.length;
+    
     // calculate the total of all the items in the cart
-    const amounts = this.list.map((item) => item.FinalPrice);
-    this.itemTotal = amounts.reduce((sum, item) => sum + item);
-    summaryElement.innerText = `$${this.itemTotal}`;;
+    this.itemTotal = this.list.reduce((sum, item) => sum + Number(item.FinalPrice || item.price || 0), 0);
+
+    itemNumElement.innerText = this.list.length;
+    
+    summaryElement.innerText = `$${this.itemTotal.toFixed(2)}`;
   }
 
   calculateOrderTotal() {
     // calculate the shipping and tax amounts. Then use them to along with the cart total to figure out the order total
+  
     this.tax = (this.itemTotal * .06);
     this.shipping = 10 + (this.list.length - 1) * 2;
     this.orderTotal = (
